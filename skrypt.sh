3a53edbcac5650bb898445eeeb2f84c0ea2eeedb
Kamil <dsw56045@student.dsw.edu.pl>
2025-06-08 18:10:20 +0200

    Dodano flagę --logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 
+case "$1" in
 if [[ "$1" == "--date" || "$1" == "-d" ]]; then
   date
 fi
+
+  --logs|-l)
+    count=${2:-100}
+    for i in $(seq 1 "$count"); do
+      echo "Plik: log$i.txt | Skrypt: $0 | Data: $(date)" > "log$i.txt"
+    done
+    echo "Utworzono $count plików logx.txt"
+    ;;
+
